f7b8416866215265fbc3ee5fcad243ea717f59b4
sohnjun96 <sohnjun96@gmail.com>
2025-04-01 23:08:00 +0900

    Update 4_🎈_통계.py

--- pages/4_🎈_통계.py
+++ pages/4_🎈_통계.py
@@ -68,16 +68,52 @@ for 선수 in 등록선수:
 통계_전체.set_index("이름", inplace=True)
 
 # 여기서부터 표시코드
-st.header(":file_folder:전체 통계")
+st.header(":file_folder: 전체 통계")
 
-# with st.container(border=True):
-seg_select = st.segmented_control("", ["승률", "승리 수", "최고 경기"], default="승률")
+tab1, tab2, tab3 = st.tabs(["랭킹", "경기", "대회"])
 
-if seg_select == "승률":
-    st.write(통계_전체[["전체_승률", "전체_경기수","전체_승리수", "전체_패배수"]].sort_values('전체_승률', ascending=False))
-elif seg_select == "승리 수":
-    st.write(통계_전체[["전체_승률", "전체_경기수","전체_승리수", "전체_패배수"]].sort_values('전체_승리수', ascending=False))
-else:
-    st.write(통계_전체)
+with tab1:
+    
+    st.write(통계_전체[["랭킹_현재", "ELO 현재","ELO 최고", "ELO 최저"]].sort_values('랭킹_현재', ascending=True))
+
+
+with tab2:
+    종류_select = st.pills("종류", ["승률", "승리 수", "경기 수"], default="승률")
+    단복_select = st.segmented_control("단식복식", ["전체", "단식", "복식"], default="전체")
+    
+    df1 = 통계_전체[["전체_승률", "전체_경기수","전체_승리수", "전체_패배수"]].rename(columns={"전체_승률":"승률","전체_경기수":"경기", "전체_승리수":"승리", "전체_패배수":"패배"})
+    df2 = 통계_전체[["단식_승률", "단식_경기수","단식_승리수", "단식_패배수"]].rename(columns={"단식_승률":"승률","단식_경기수":"경기", "단식_승리수":"승리", "단식_패배수":"패배"})
+    df3 = 통계_전체[["복식_승률", "복식_경기수","복식_승리수", "복식_패배수"]].rename(columns={"복식_승률":"승률","복식_경기수":"경기", "복식_승리수":"승리", "복식_패배수":"패배"})
+    config = {"승률":st.column_config.NumberColumn(
+            format="%d %%",
+        )}
+    if 종류_select == "승률":
+
+        if 단복_select == "전체":
+            st.dataframe(df1.sort_values('승률', ascending=False), column_config=config)
+        elif 단복_select == "단식":
+            st.dataframe(df2.sort_values('승률', ascending=False), column_config=config)
+        elif 단복_select == "복식":
+            st.dataframe(df3.sort_values('승률', ascending=False), column_config=config)
+
+    elif 종류_select == "승리 수":
+        if 단복_select == "전체":
+            st.dataframe(df1.sort_values('승리', ascending=False), column_config=config)
+        elif 단복_select == "단식":
+            st.dataframe(df2.sort_values('승리', ascending=False), column_config=config)
+        elif 단복_select == "복식":
+            st.dataframe(df3.sort_values('승리', ascending=False), column_config=config)
+
+    else:
+        if 단복_select == "전체":
+            st.dataframe(df1.sort_values('경기', ascending=False), column_config=config)
+        elif 단복_select == "단식":
+            st.dataframe(df2.sort_values('경기', ascending=False), column_config=config)
+        elif 단복_select == "복식":
+            st.dataframe(df3.sort_values('경기', ascending=False), column_config=config)
     
     
+with tab3:
+    경기결과 = games_hist.sort_values('날짜', ascending=False)
+    경기결과.set_index("대회명", inplace=True)
+    st.write(경기결과)
